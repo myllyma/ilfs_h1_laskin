f9b83c36364a57faf672744e3361db56d64e9483
myllyma <matti.myllymaki83@gmail.com>
2020-10-21 13:16:59 +0300

    more cleanup

--- src/App.js
+++ src/App.js
@@ -7,40 +7,41 @@ import stringMath from 'string-math';
 function App() {
   const [calculatorField, setCalculatorField] = useState("0");
 
-  const updateOutput = (input) => {
+  const processCalculatorFieldUpdate = (input) => {
     calculatorField === "0" ? setCalculatorField(input) : setCalculatorField(calculatorField.concat(input));
   }
 
-  const buttonClicked = (pressedButton) => () => {
+  // Handle button presses.
+  const handleButtonClick = (pressedButton) => () => {
     switch (pressedButton) {
       case '1':
-        updateOutput("1"); break;
+        processCalculatorFieldUpdate("1"); break;
       case '2':
-        updateOutput("2"); break;
+        processCalculatorFieldUpdate("2"); break;
       case '3':
-        updateOutput("3"); break;
+        processCalculatorFieldUpdate("3"); break;
       case '4':
-        updateOutput("4"); break;
+        processCalculatorFieldUpdate("4"); break;
       case '5':
-        updateOutput("5"); break;
+        processCalculatorFieldUpdate("5"); break;
       case '6':
-        updateOutput("6"); break;
+        processCalculatorFieldUpdate("6"); break;
       case '7':
-        updateOutput("7"); break;
+        processCalculatorFieldUpdate("7"); break;
       case '8':
-        updateOutput("8"); break;
+        processCalculatorFieldUpdate("8"); break;
       case '9':
-        updateOutput("9"); break;
+        processCalculatorFieldUpdate("9"); break;
       case '0':
-        updateOutput("0"); break;
+        processCalculatorFieldUpdate("0"); break;
       case '+': // Addition
-        updateOutput("+"); break;
+        processCalculatorFieldUpdate("+"); break;
       case '-': // Subtraction
-        updateOutput("-"); break;
+        processCalculatorFieldUpdate("-"); break;
       case '*': // Multiplication
-        updateOutput("*"); break;
+        processCalculatorFieldUpdate("*"); break;
       case '/': // Division
-        updateOutput("/"); break;
+        processCalculatorFieldUpdate("/"); break;
       case 'c': // Clear
         setCalculatorField("0"); break;
       case 'e': // Enter
@@ -58,22 +59,22 @@ function App() {
   return (
     <div className="App">
       <input type="text" value={calculatorField} onChange={handleCalculatorFieldChange} className="calc_input"></input>
-      <button onClick={buttonClicked('+')} className="button">+</button>
-      <button onClick={buttonClicked('-')} className="button">-</button>
-      <button onClick={buttonClicked('*')} className="button">*</button>
-      <button onClick={buttonClicked('/')} className="button">/</button>
-      <button onClick={buttonClicked('7')} className="button">7</button>
-      <button onClick={buttonClicked('8')} className="button">8</button>
-      <button onClick={buttonClicked('9')} className="button">9</button>
-      <button onClick={buttonClicked('c')} className="button clear">C</button>
-      <button onClick={buttonClicked('4')} className="button">4</button>
-      <button onClick={buttonClicked('5')} className="button">5</button>
-      <button onClick={buttonClicked('6')} className="button">6</button>
-      <button onClick={buttonClicked('e')} className="button enter">enter</button>
-      <button onClick={buttonClicked('1')} className="button">1</button>
-      <button onClick={buttonClicked('2')} className="button">2</button>
-      <button onClick={buttonClicked('3')} className="button">3</button>
-      <button onClick={buttonClicked('0')} className="button zero">0</button>
+      <button onClick={handleButtonClick('+')} className="button">+</button>
+      <button onClick={handleButtonClick('-')} className="button">-</button>
+      <button onClick={handleButtonClick('*')} className="button">*</button>
+      <button onClick={handleButtonClick('/')} className="button">/</button>
+      <button onClick={handleButtonClick('7')} className="button">7</button>
+      <button onClick={handleButtonClick('8')} className="button">8</button>
+      <button onClick={handleButtonClick('9')} className="button">9</button>
+      <button onClick={handleButtonClick('c')} className="button clear">C</button>
+      <button onClick={handleButtonClick('4')} className="button">4</button>
+      <button onClick={handleButtonClick('5')} className="button">5</button>
+      <button onClick={handleButtonClick('6')} className="button">6</button>
+      <button onClick={handleButtonClick('e')} className="button enter">enter</button>
+      <button onClick={handleButtonClick('1')} className="button">1</button>
+      <button onClick={handleButtonClick('2')} className="button">2</button>
+      <button onClick={handleButtonClick('3')} className="button">3</button>
+      <button onClick={handleButtonClick('0')} className="button zero">0</button>
     </div>
   );
 }
